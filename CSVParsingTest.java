577c384e05594b86f97f15b21636b43d4bc37a06
LongD <60275023+bouyal@users.noreply.github.com>
2022-02-01 14:37:47 -0600

    Create CSVParsingTest.java

---
+++ CSVParsingTest.java
@@ -0,0 +1,21 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+
+public class CSVParsingTest{
+    public static void main(String[] args) throws IOException {
+        String csvFile = "test.csv";
+        try (Reader reader = new FileReader(csvFile);
+            CSVPrinter csvPrinter = new CSVPrinter(reader, CSVFormat.DEFAULT);) {
+            Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+            for (CSVRecord csvRecord : csvRecords) {
+                System.out.println("English: " +csvRecord.get(0) + " to Spanish: "+ csvRecord.get(1));
+            }
+            
+        }
+    }
+}
